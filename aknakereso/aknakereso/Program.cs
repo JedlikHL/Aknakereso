bb4ee7c725c213df4ac493aa185dce44610c1a37
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-04 13:33:01 +0100

    Feltöltés

--- aknakereso/aknakereso/Program.cs
+++ aknakereso/aknakereso/Program.cs
@@ -8,10 +8,13 @@ namespace aknakereso
 {
     class Program
     {
+        char [,] pálya =new char[10,10];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Feltöltés();
+
             Console.ReadKey();
         }
+        
     }
 }
