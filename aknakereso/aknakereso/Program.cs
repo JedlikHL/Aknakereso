30ece0fec566e10de9edbf77d03f65af20dbd070
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-11 13:03:18 +0100

    Alex munkája

--- aknakereso/aknakereso/Program.cs
+++ aknakereso/aknakereso/Program.cs
@@ -4,72 +4,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace aknakereso
+namespace Aknakereso
 {
     class Program
     {
-        
+        //Készíts fvényt, ami megadja, hogy az adott hely szomszédjában hány bomba van!
         static void Main(string[] args)
         {
             char[,] pálya = new char[10, 10];
             Feltöltés(pálya);
-            Kirajzoló(pálya);
-            Tippelő(pálya);
+            BomBasorsoló(pálya);
+            Kirajzoló(pálya, false);
+            int lépx;
+            int lépy;
+            do
+            {
+                Lépés(pálya, out lépx, out lépy);
+            } while (pálya[lépx, lépy] != 'B');
 
             Console.ReadKey();
         }
         static void Feltöltés(char[,] pálya)
         {
             for (int i = 0; i < pálya.GetLength(0); i++)
-			{
-               for (int j = 0; j < pálya.GetLength(1); j++)
-               {
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
+                {
                     pálya[i, j] = '_';
-               }
-			}
+                }
+            }
+        }
+        static void BomBasorsoló(char[,] pálya)
+        {
             Random gép = new Random();
+            Console.WriteLine("Add meg a bombák számát:");
+            int bombaSzám = int.Parse(Console.ReadLine());
             int sor, oszlop;
-            
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < bombaSzám; i++)
             {
                 do
                 {
                     sor = gép.Next(10);
                     oszlop = gép.Next(10);
-
-                } while (pálya[sor,oszlop]=='B');
+                } while (pálya[sor, oszlop] == 'B');
                 pálya[sor, oszlop] = 'B';
             }
         }
-        static void Kirajzoló(char[,]pálya)
+        static void Kirajzoló(char[,] pálya, bool legyenBomba)
         {
-            for (int sor = 0; sor < pálya.GetLength(0); sor++)
+            for (int i = 0; i < pálya.GetLength(0); i++)
             {
-                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                for (int j = 0; j < pálya.GetLength(1); j++)
                 {
-                    Console.Write($"{pálya[sor,oszlop]}|");
+                    if (legyenBomba)
+                    {
+                        Console.Write($"{pálya[i, j]}");
+                    }
+                    else if (pálya[i, j] != 'X')
+                    {
+                        Console.Write('_');
+                    }
+                    Console.Write('|');
                 }
                 Console.WriteLine();
             }
-        }
-        static void Tippelő(char[,]pálya)
-        {
-            Console.WriteLine("Kérem a sor tippet 1-10 között:");
-            int sortipp = int.Parse(Console.ReadLine());
-            Console.WriteLine("Kérem az oszlop tippet 1-10 között:");
-            int oszloptipp = int.Parse(Console.ReadLine());
-            if (sortipp<10 && oszloptipp<10)
-	        {
-                int sor,oszlop;
-                do
-                {
-                    sor = sortipp-1;
-                    oszlop = oszloptipp-1;
 
-                } while (pálya[sor,oszlop]=='X');
-                pálya[sor, oszlop] = 'X';
-	        }
-            Kirajzoló(pálya);
         }
+        static void Lépés(char[,] pálya, out int lépx, out int lépy)
+        {
+            Console.WriteLine("Kérem a sort:");
+            lépx = int.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("Kérem az oszlopot:");
+            lépy = int.Parse(Console.ReadLine()) - 1;
+            if (pálya[lépx, lépy] == 'B')
+            {
+                Kirajzoló(pálya, true);
+                Console.WriteLine("Felrobbantál!");
+            }
+            else
+            {
+                pálya[lépx, lépy] = 'X';
+                Kirajzoló(pálya, false);
+            }
+
+        }
+
     }
 }
